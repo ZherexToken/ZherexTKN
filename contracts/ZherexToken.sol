5d155dba827458c225f05dcbba8e55f1dd99e558
ZherexToken <ZherexToken@gmail.com>
2019-04-16 15:26:43 +0200

    First commit

---
+++ contracts/ZherexToken.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract ZherexToken is ERC20 {
+
+	string public name = "ZherexToken";
+	string public symbol = "TT";
+	uint8 public decimals = 2;
+	uint public INITIAL_SUPPLY = 12000;
+
+	constructor() public {
+		_mint(msg.sender, INITIAL_SUPPLY);
+	}
+
+
+
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var ZherexToken = artifacts.require("ZherexToken");
+
+module.exports = function(deployer) {
+	deployer.deploy(ZherexToken);
+}
